a1f02d776b6ecd8f60afb09a5fa6e8644203cda0
Rohit <rohit50dx@gmail.com>
2025-11-18 16:31:20 +0530

    add atlas online db

---
+++ mongodb-atlas-example.js
@@ -0,0 +1,175 @@
+import express from 'express'
+import { MongoClient, ObjectId } from 'mongodb';
+
+const dbName = "collage"
+const url = "mongodb+srv://cluster0.qbz6cix.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(url, {
+    auth: {
+        username: "rohit50dx",
+        password: "Test@123"
+    }
+});
+
+
+
+const app = express();
+app.set("view engine", 'ejs')
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+client.connect().then((connection) => {
+ const db = connection.db(dbName);
+
+
+// API to get all students  
+ app.get("/api", async (req, resp) => {
+        const collection = db.collection("students");
+        const students = await collection.find().toArray();
+        resp.status(200).json({
+            status: resp.statusCode,
+            message: "Students fetched successfully",
+            data: students,
+        });
+    });
+
+// Route to render all students
+    app.get("/", async (req, resp) => {
+        const collection = db.collection("students")
+        const students = await collection.find().toArray();
+        resp.render("usersCard", { users: students });
+
+    })
+
+ // Routes to add a student   
+    app.get("/add-student", async (req, resp) => {
+        resp.render("addStudent");
+    })
+
+
+// POST: Add student    
+    app.post("/add-student", async (req, resp) => {
+        const { name, age, email } = req.body;
+
+        if (!name || name.trim() === "") {
+            resp.send({ status: resp.statusCode, message: "name is required" })
+            return false;
+        }
+        if (!age || isNaN(age) || age < 1) {
+            resp.send({ status: resp.statusCode, message: "age is required" })
+            return false;
+        }
+        if (!email || !email.includes("@") || !email.includes(".")) {
+            resp.send({ status: resp.statusCode, message: "email is required" })
+            return false;
+        }
+
+        const collection = db.collection("students")
+        await collection.insertOne({ name, age, email })
+        // await collection.insertOne(req.body) //you can also use this for insert the record
+        resp.redirect("/")
+    })
+
+
+
+ // API: POST Add student   
+    app.post("/add-studend-api", async (req, resp) => {
+        const { name, age, email } = req.body;
+        if (!name || name.trim() === "") {
+            resp.send({ status: resp.statusCode, message: "name is required" })
+            return false;
+        }
+        if (!age || isNaN(age) || age < 1) {
+            resp.send({ status: resp.statusCode, message: "age is required" })
+            return false;
+        }
+        if (!email || !email.includes("@") || !email.includes(".")) {
+            resp.send({ status: resp.statusCode, message: "email is required" })
+            return false;
+        }
+        const collection = db.collection("students")
+        await collection.insertOne({ name, age, email })
+        resp.send({ status: resp.statusCode, message: "Student added successfully" })
+    })
+
+
+
+// DELETE: Delete student
+    app.delete("/delete-student/:id", async (req, resp) => {
+        const id = req.params.id;
+        console.log("Deleting student with ID:", id);
+        try {
+            const collection = db.collection("students");
+            const result = await collection.deleteOne({ _id: new ObjectId(id) });
+            if (result.deletedCount === 1) {
+                return resp.status(200).json({ status: resp.statusCode, message: "Student successfully deleted" });
+            } else {
+                return resp.status(404).json({ status: resp.statusCode, message: "Student not found" });
+            }
+        } catch (err) {
+            console.error("Delete error:", err);
+            return resp.status(500).json({ status: resp.statusCode, message: "Internal Server error" });
+        }
+    });
+
+
+
+// PUT: Update student
+    app.put("/update-student/:id", async (req, resp) => {
+
+        const id = req.params.id;
+        console.log("Update request ID:", id);
+        const { name, age, email } = req.body;
+
+        // Validation
+        if (!name?.trim()) return resp.status(400).json({ status: resp.statusCode, message: "Name is required" });
+        if (!age || isNaN(age) || age < 1) return resp.status(400).json({ status: resp.statusCode, message: "Age is required" });
+        if (!email || !email.includes("@")) return resp.status(400).json({ status: resp.statusCode, message: "Email is required" });
+
+        try {
+            const collection = db.collection("students");
+            const result = await collection.updateOne(
+                { _id: new ObjectId(id) },
+                { $set: { name: name.trim(), age: parseInt(age), email: email.trim().toLowerCase() } }
+            );
+
+            if (result.matchedCount === 1) {
+                return resp.status(200).json({ status: resp.statusCode, message: "Student successfully updated" });
+
+            } else {
+                return resp.status(404).json({ status: resp.statusCode, message: "Student not found" });
+            }
+        } catch (err) {
+            console.error("Update error:", err);
+            return resp.status(500).json({ status: resp.statusCode, message: "Internal Server error" });
+        }
+    });
+
+
+// GET: Student details by ID
+    app.get("/student-details/:id", async (req, resp) => {
+        try {
+            const id = req.params.id;
+            console.log(" request ID:", id);
+            const collection = db.collection("students");
+            const student = await collection.findOne(
+                { _id: new ObjectId(id) },
+            );
+            if (student) {
+                return resp.status(200).json({ status: resp.statusCode, message: "Student Data Fetched successfully", data: student });
+            } else {
+                return resp.status(404).json({ status: resp.statusCode, message: "Student not found" });
+            }
+        } catch (err) {
+            console.error("Fetch error:", err);
+            return resp.status(500).json({ status: resp.statusCode, message: "Internal Server error" });
+        }
+    })
+})
+
+
+
+// app.set("view engine",'ejs')
+
+app.listen(8080)
+
